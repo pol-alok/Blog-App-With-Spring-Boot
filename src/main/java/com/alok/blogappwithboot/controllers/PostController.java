31f69ce45c16424849e6d534d8ee484fbc8dc0ed
ravi <ravikantpal337@gmail.com>
2020-01-10 00:35:33 +0530

    ok

--- src/main/java/com/alok/blogappwithboot/controllers/PostController.java
+++ src/main/java/com/alok/blogappwithboot/controllers/PostController.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.*;
 
 
 import java.security.Principal;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
